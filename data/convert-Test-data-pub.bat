c64058d99bbcf4e2f3bd0d045ada6b9251e97edf
Christo Radev <christo.radev@gmail.com>
2025-03-21 15:23:57 +0200

    Delete data/convert-Test-data-pub.bat

--- data/convert-Test-data-pub.bat
+++
@@ -1,6 +0,0 @@
-call xlsx2csv Test-data-pub.xlsx
-call xlsx2json Test-data-pub.xlsx
-rem echo Edit Test-data-pub.csv ...
-rem pause
-rem call csv2json Test-data-pub.csv -o Test-data-pub.json -f ","
-pause
